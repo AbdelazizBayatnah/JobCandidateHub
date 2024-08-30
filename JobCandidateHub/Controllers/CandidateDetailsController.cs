154da17dfe032471f23f50735741beba3f0d7493
Abdelaziz Bayatnah <abdelaziz.bayatnah@gmail.com>
2024-08-30 14:46:54 +0300

    Initialize the project, create the model and start implementing the controller

---
+++ JobCandidateHub/Controllers/CandidateDetailsController.cs
@@ -0,0 +1,28 @@
+﻿using JobCandidateHub.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace JobCandidateHub.Controllers
+{
+    [Route("api/{controller}")]
+    [ApiController]
+    public class CandidateDetailsController : ControllerBase
+    {
+        public CandidateDetailsController() { }
+
+
+        /// <summary>
+        ///  Post or Put Candidate Details
+        /// </summary>
+        /// <response code="404">Not found</response>
+        /// <response code="422">The model is not valid.</response>
+        /// <response code="500">Server error.</response>
+        /// <returns></returns>
+        [HttpPost]
+
+        public async Task<ActionResult<CandidateDetails>> CreateCandidateDetails(
+            [FromBody] CandidateDetails candidateDetailsModel)
+        {
+            return Ok("");
+        }
+    }
+}
---
+++ JobCandidateHub/Models/CandidateDetails.cs
@@ -0,0 +1,30 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace JobCandidateHub.Models
+{
+    public class CandidateDetails
+    {
+        [Key]
+        [Required]
+        public string Email { get; set; } = null!;
+
+        [Required]
+        public string FirstName { get; set; } = null!;
+
+        [Required]
+        public string LastName { get; set; } = null!;
+
+        public string? PhoneNumber { get; set; }
+
+        public DateTime AvailableForCallFrom { get; set; }
+
+        public DateTime AvailableFroCallTo { get; set; }
+
+        public string? LinkedInProfileUrl { get; set; }
+
+        public string? GitHubProfileUrl { get; set; }
+
+        [Required]
+        public string FreeTextComment { get; set; } = null!;
+    }
+}
