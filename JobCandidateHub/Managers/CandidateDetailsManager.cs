9a2f5671395537e39ddb28c96483b33d3bc0474a
Abdelaziz Bayatnah <abdelaziz.bayatnah@gmail.com>
2024-08-30 15:08:58 +0300

    Add csv file service, and start implementing the Candidate Details Manager that handle the business logic flow

---
+++ JobCandidateHub/Managers/CandidateDetailsManager.cs
@@ -0,0 +1,29 @@
+﻿using JobCandidateHub.Models;
+
+namespace JobCandidateHub.Managers
+{
+    public interface ICandidateDetailsManager
+    {
+        Task<CandidateDetails?> CreateCandidateDetailsAsync(CandidateDetails candidateDetailsModel);
+
+        Task<CandidateDetails?> UpdateCandidateDetailsAsync(CandidateDetails newCandidateDetailsModel);
+    }
+
+    public class CandidateDetailsManager : ICandidateDetailsManager
+    {
+        public CandidateDetailsManager()
+        {
+
+        }
+
+        public Task<CandidateDetails?> CreateCandidateDetailsAsync(CandidateDetails candidateDetailsModel)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<CandidateDetails?> UpdateCandidateDetailsAsync(CandidateDetails newCandidateDetailsModel)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ JobCandidateHub/Services/CsvFileService.cs
@@ -0,0 +1,47 @@
+﻿using JobCandidateHub.Models;
+using System.Formats.Asn1;
+using System.Globalization;
+using CsvHelper;
+using CsvHelper.Configuration;
+
+namespace JobCandidateHub.Services
+{
+    public interface ICsvFileService
+    {
+        Task<List<CandidateDetails>> GetAllCandidateDetails();
+
+        Task SaveCandidateDetails(List<CandidateDetails> candidateDetailsModels);
+    }
+
+    public class CsvFileService : ICsvFileService
+    {
+        private readonly string _csvFilePath;
+
+        public CsvFileService(string csvFilePath)
+        {
+            _csvFilePath = csvFilePath;
+        }
+
+        public async Task<List<CandidateDetails>> GetAllCandidateDetails()
+        {
+            if (!File.Exists(_csvFilePath))
+            {
+                return new List<CandidateDetails>();
+            }
+            using var reader = new StreamReader(_csvFilePath);
+
+            using var csv = new CsvReader(reader, new CsvConfiguration(CultureInfo.InvariantCulture));
+
+            return await Task.Run(() => csv.GetRecords<CandidateDetails>().ToList());
+        }
+
+        public async Task SaveCandidateDetails(List<CandidateDetails> candidateDetailsModels)
+        {
+            await using var writer = new StreamWriter(_csvFilePath, false);
+
+            await using var csv = new CsvWriter(writer, new CsvConfiguration(CultureInfo.InvariantCulture));
+
+            await Task.Run(() => csv.WriteRecords(candidateDetailsModels));
+        }
+    }
+}
